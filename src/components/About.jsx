4dc4aefc0bf7d233b9515537904458feb652542f
Stevan Nikolic <stevannikolic1992@yahoo.com>
2025-05-17 12:23:23 +1000

    feat: add About & update Hero

---
+++ src/components/About.jsx
@@ -0,0 +1,70 @@
+const techStackArray = [
+  "HTML",
+  "CSS",
+  "Tailwind",
+  "Material UI",
+  "Bootstrap",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Redux",
+  "Node.js",
+  "Express.js",
+  "Firebase",
+  "axios",
+];
+
+function About() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-[radial-gradient(circle,_#ffffff,_#e0e0e0,_#c0c0c0)]">
+      <div className="flex flex-col justify-center items-center text-center w-full max-w-[1200px] p-[2rem] shadow-[0_0_20px_rgba(0,0,0,0.5)] rounded-lg">
+        <h2 className="text-gray-900 text-[4rem] uppercase">About Me</h2>
+        <h4 className="text-gray-900 text-[1.75rem]">
+          My name is Stevan Nikolic, Web Developer based in Sydney, Australia.
+          Outside of coding, I enjoy football, darts, board games, video games,
+          and spending time with friends.
+          <br /> These hobbies keep me balanced and inspired.
+        </h4>
+        <div className="mt-12 flex flex-col gap-16 lg:flex-row text-left w-full">
+          <div className="flex-1 space-y-6">
+            <h2 className="text-2xl font-semibold text-gray-800 text-center">
+              A little bit about why programming:
+            </h2>
+            <h3 className="text-gray-600 text-[1.25rem] ">
+              I am genuinely fascinated by the entire world of programming —
+              from understanding how the internet works to seeing its impact in
+              fields like medicine and beyond. For me, programming is more than
+              just a job; it’s a true passion and a way to explore endless
+              possibilities. I love solving problems, building useful tools, and
+              constantly pushing my limits to learn new technologies. This deep
+              enthusiasm drives me to create meaningful digital experiences that
+              can make a difference.
+            </h3>
+            <a href="#contact" className="hero-link">
+              Contact
+            </a>
+          </div>
+          <div className="flex-1 items-center space-y-6">
+            <h2 className="text-2xl font-semibold text-gray-800 text-center">
+              My Skills
+            </h2>
+            <ul className="grid grid-cols-2 sm:grid-cols-3 gap-4 text-white font-medium">
+              {techStackArray.map((tech, index) => (
+                <li
+                  key={index}
+                  className="bg-[#333] text-white uppercase 
+                             py-3 rounded-lg text-center shadow-[0_5px_15px_rgba(0,0,0,0.15)]"
+                  style={{ minWidth: "120px" }}
+                >
+                  {tech}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,22 @@
 function Hero() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center  bg-[linear-gradient(to_right,rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url('./assets/hero-background.svg')] text-center px-4 gap-[60px]">
-      <h1 className="text-6xl font-extrabold text-gray-900 uppercase">
-        Hey, I'm Stevan Nikolic
-      </h1>
-      <h3 className="max-w-2xl text-lg text-gray-800">
-        Welcome to my portfolio website. I am passionate about frontend
-        development and building beautiful web apps.
-      </h3>
-      <a href="#projects" className="hero-link shake-on-hover">
-        Projects
-      </a>
+    <div className="relative h-screen flex flex-col items-center justify-center bg-[radial-gradient(circle,_#2a2a2a,_#1f1f1f,_#000000)]">
+      <div className="flex flex-col justify-center items-center text-center w-[1200px] p-[6rem] gap-[60px]  shadow-[0_0_20px_rgba(255,255,255,0.08)] rounded-lg">
+        <h1 className="hero-heading">Hi, I'm Stevan</h1>
+        <h3 className="hero-secondary">
+          Welcome to my little corner of the internet. Whether you're here by
+          choice or cosmic accident — enjoy the scroll. If my work speaks to you
+          (or whispers mildly), feel free to reach out. I'm just one message
+          away and surprisingly friendly. Let's build something awesome!
+        </h3>
+        <a href="#projects" className="hero-link shake-on-hover">
+          Projects
+        </a>
+      </div>
+      {/* Bottom page loader */}
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center">
+        <span className="loader" />
+      </div>
     </div>
   );
 }
